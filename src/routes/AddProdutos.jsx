05d83a8e9a1796dfd32069e8f4be66b87e6fc2d7
LeonardoBuonanno <rm98315@fiap.com.br>
2023-09-15 19:27:04 -0300

    crianda a funlçao de adicionar produtos sjx

--- src/routes/AddProdutos.jsx
+++ src/routes/AddProdutos.jsx
@@ -1,6 +1,17 @@
+import { ListaProdutos } from "../Components/ListaProdutos"
 export default function AddProdutos(){
 
     return(
-        <h1>Olá</h1>
+        <div>
+      <h1>Minha Lista de Produtos</h1>
+      <ul>
+        {ListaProdutos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <button onClick={() => adicionarItem('Novo Item')}>
+        Adicionar Item
+      </button>
+    </div>
     )
 }
